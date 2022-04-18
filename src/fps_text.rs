29a0f57d6a2f6a47f34b05c10de068d5eefd05dd
reiya0104 <reiya0104@ezweb.ne.jp>
2022-04-19 02:43:58 +0900

    update: format text

--- src/fps_text.rs
+++ src/fps_text.rs
@@ -41,7 +41,10 @@ pub(crate) fn setup_fps_text(mut commands: Commands, asset_server: Res<AssetServ
         .insert(FpsText);
 }
 
-pub(crate) fn text_update_system(diagnostics: Res<Diagnostics>, mut query: Query<&mut Text, With<FpsText>>) {
+pub(crate) fn text_update_system(
+    diagnostics: Res<Diagnostics>,
+    mut query: Query<&mut Text, With<FpsText>>,
+) {
     for mut text in query.iter_mut() {
         if let Some(fps) = diagnostics.get(FrameTimeDiagnosticsPlugin::FPS) {
             if let Some(average) = fps.average() {

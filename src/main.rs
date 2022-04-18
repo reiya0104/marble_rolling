fb3d05157feba92ca6c6a0424d8d41e04947ef88
reiya0104 <reiya0104@ezweb.ne.jp>
2022-04-18 18:26:15 +0900

    add: marble, camera and debug_position system

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,56 @@
+pub mod component;
+
+use bevy::{prelude::*, DefaultPlugins};
+use component::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(setup)
+        .add_system(debug_position)
+        .run();
+}
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    // camera
+    let camera_position = Position {
+        vec3: Vec3::new(0.0, 0.0, 5.0),
+    };
+    commands
+        .spawn()
+        .insert(Camera)
+        .insert(camera_position.clone())
+        .insert_bundle(PerspectiveCameraBundle {
+            transform: camera_position.into(),
+            ..default()
+        });
+
+    // marble
+    let marble_position = Position::default();
+    commands
+        .spawn()
+        .insert(Marble)
+        .insert(marble_position.clone())
+        .insert_bundle(PbrBundle {
+            mesh: meshes.add(Mesh::from(shape::UVSphere {
+                radius: 0.5,
+                ..default()
+            })),
+            material: materials.add(StandardMaterial {
+                base_color: Color::LIME_GREEN,
+                ..default()
+            }),
+            transform: marble_position.into(),
+            ..default()
+        });
+}
+
+fn debug_position(query: Query<(&Position, Entity)>) {
+    for (position, entity) in query.iter() {
+        println!("{:?}, {:?}", position, entity);
+    }
 }

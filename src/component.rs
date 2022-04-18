d4ec846a752e54ef6fb5ebbe379960cc27e5e9f0
reiya0104 <reiya0104@ezweb.ne.jp>
2022-04-19 01:18:05 +0900

    add: gravity system

--- src/component.rs
+++ src/component.rs
@@ -1,15 +1,51 @@
 use bevy::prelude::*;
 
 // オブジェクト描画用コンポーネント
-#[derive(Debug, Component)]
-struct ObjectView;
+#[derive(Debug, Default, Clone, Component)]
+pub(crate) struct ObjectView {
+    pub(crate) position: Vec3,
+}
+
+impl ObjectView {
+    pub(crate) fn new(position_vec3: Vec3) -> Self {
+        Self {
+            position: position_vec3,
+        }
+    }
+
+    pub(crate) fn from_position(position: Position) -> Self {
+        Self {
+            position: position.vec3,
+        }
+    }
+}
 
 // 位置情報などのコンポーネント
-#[derive(Debug, Component)]
-struct Acceleration;
+#[derive(Debug, Default, Clone, Component)]
+pub(crate) struct Acceleration {
+    pub(crate) vec3: Vec3,
+}
 
-#[derive(Debug, Component)]
-struct Velocity;
+impl Acceleration {
+    pub(crate) fn new(x: f32, y: f32, z: f32) -> Self {
+        Self {
+            vec3: Vec3::new(x, y, z),
+        }
+    }
+}
+
+#[derive(Debug, Default, Clone, Component)]
+pub(crate) struct Velocity {
+    pub(crate) vec3: Vec3,
+}
+
+impl Velocity {
+    pub(crate) fn new(x: f32, y: f32, z: f32) -> Self {
+        Self {
+            vec3: Vec3::new(x, y, z),
+        }
+    }
+}
 
 #[derive(Debug, Default, Clone, Component)]
 pub(crate) struct Position {
--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,30 @@ use std::f32::consts::PI;
 use bevy::{prelude::*, DefaultPlugins};
 use component::*;
 
+const GRAVITY: f32 = 9.80665;
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_startup_system(setup)
-        .add_system(debug_position)
+        // .add_system(debug_position)
+        .add_system(update_velocity_by_acceleration.label("update_velocity_by_acceleration"))
+        .add_system(
+            update_position_by_velocity
+                .label("update_position_by_velocity")
+                .after("update_velocity_by_acceleration"),
+        )
+        .add_system(
+            update_object_view_by_position
+                .label("update_object_view_by_position")
+                .after("update_position_by_velocity"),
+        )
+        .add_system(
+            update_transform_by_object_view
+                .label("update_transform_by_object_view")
+                .after("update_object_view_by_position"),
+        )
+        .add_system(create_marble)
         .run();
 }
 
@@ -56,11 +75,18 @@ fn setup(
         });
 
     // marble
-    let marble_position = Position::new(0.0, 2.0, 0.0);
+    let gravity = GRAVITY / 10.0;
+    let marble_position = Position::new(0.0, 4.0, 0.0);
+    let marble_acceleration = Acceleration::new(0.0, -gravity, 0.0);
+    let marble_velocity = Velocity::default();
+
     commands
         .spawn()
         .insert(Marble)
+        .insert(marble_acceleration.clone())
+        .insert(marble_velocity.clone())
         .insert(marble_position.clone())
+        .insert(ObjectView::from_position(marble_position.clone()))
         .insert_bundle(PbrBundle {
             mesh: meshes.add(Mesh::from(shape::UVSphere {
                 radius: 0.5,
@@ -71,6 +97,28 @@ fn setup(
                 ..default()
             }),
             transform: marble_position.into(),
+            ..default()
+        });
+
+    // marble2
+    let marble_position = Position::new(1.0, 5.0, 1.0);
+    commands
+        .spawn()
+        .insert(Marble)
+        .insert(marble_acceleration.clone())
+        .insert(marble_velocity.clone())
+        .insert(marble_position.clone())
+        .insert(ObjectView::from_position(marble_position.clone()))
+        .insert_bundle(PbrBundle {
+            mesh: meshes.add(Mesh::from(shape::UVSphere {
+                radius: 0.5,
+                ..default()
+            })),
+            material: materials.add(StandardMaterial {
+                base_color: Color::LIME_GREEN,
+                ..default()
+            }),
+            transform: marble_position.clone().into(),
             ..default()
         });
 
@@ -89,43 +137,109 @@ fn setup(
             transform: light_position.into(),
             ..default()
         });
-    
-    commands.spawn()
-        .insert_bundle(ImageBundle {
-            style: Style {
-                size: Size::new(Val::Px(150.0), Val::Px(150.0)),
-                position_type: PositionType::Absolute,
-                position: Rect {
-                    right: Val::Px(50.0),
-                    bottom: Val::Px(50.0),
-                    ..default()
-                },
+
+    commands.spawn().insert_bundle(ImageBundle {
+        style: Style {
+            size: Size::new(Val::Px(150.0), Val::Px(150.0)),
+            position_type: PositionType::Absolute,
+            position: Rect {
+                right: Val::Px(50.0),
+                bottom: Val::Px(50.0),
                 ..default()
             },
-            image: asset_server.load("image/mouse_controller_base.png").into(),
             ..default()
-        });
+        },
+        image: asset_server.load("image/mouse_controller_base.png").into(),
+        ..default()
+    });
 
-
-    commands.spawn()
-        .insert_bundle(ImageBundle {
-            style: Style {
-                size: Size::new(Val::Px(100.0), Val::Px(100.0)),
-                position_type: PositionType::Absolute,
-                position: Rect {
-                    right: Val::Px(50.0),
-                    bottom: Val::Px(50.0),
-                    ..default()
-                },
+    commands.spawn().insert_bundle(ImageBundle {
+        style: Style {
+            size: Size::new(Val::Px(100.0), Val::Px(100.0)),
+            position_type: PositionType::Absolute,
+            position: Rect {
+                right: Val::Px(50.0),
+                bottom: Val::Px(50.0),
                 ..default()
             },
-            image: asset_server.load("image/mouse_controller_main.png").into(),
             ..default()
-        });
+        },
+        image: asset_server.load("image/mouse_controller_main.png").into(),
+        ..default()
+    });
 }
 
 fn debug_position(query: Query<(&Position, Entity)>) {
     for (position, entity) in query.iter() {
         println!("{:?}, {:?}", position, entity);
+    }
+}
+
+fn update_velocity_by_acceleration(
+    time: Res<Time>,
+    mut query: Query<(&mut Velocity, &Acceleration, Entity)>,
+) {
+    for (mut velocity, acceleration, entity) in query.iter_mut() {
+        // println!("{:?}, {:?}, {:?}", velocity, acceleration, entity);
+        velocity.vec3 += time.delta_seconds() * acceleration.vec3;
+    }
+}
+
+fn update_position_by_velocity(
+    time: Res<Time>,
+    mut query: Query<(&mut Position, &Velocity, Entity)>,
+) {
+    for (mut position, velocity, entity) in query.iter_mut() {
+        // println!("{:?}, {:?}, {:?}", position, velocity, entity);
+        position.vec3 += time.delta_seconds() * velocity.vec3;
+    }
+}
+
+fn update_object_view_by_position(mut query: Query<(&mut ObjectView, &Position, Entity)>) {
+    for (mut object_view, position, entity) in query.iter_mut() {
+        // println!("{:?}, {:?}, {:?}", object_view, position, entity);
+        object_view.position = position.vec3;
+    }
+}
+
+fn update_transform_by_object_view(mut query: Query<(&mut Transform, &ObjectView, Entity)>) {
+    for (mut transform, object_view, entity) in query.iter_mut() {
+        // println!("{:?}, {:?}, {:?}", transform, object_view, entity);
+        transform.translation = object_view.position;
+    }
+}
+
+fn create_marble(
+    input: Res<Input<KeyCode>>,
+    input_mause: Res<Input<MouseButton>>,
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    if input.pressed(KeyCode::Space) {
+        // marbles
+        let gravity = GRAVITY / 10.0;
+        let marble_position = Position::new(0.0, 4.0, 0.0);
+        let marble_acceleration = Acceleration::new(0.0, -gravity, 0.0);
+        let marble_velocity = Velocity::default();
+        commands
+            .spawn()
+            .insert(Marble)
+            .insert(marble_acceleration.clone())
+            .insert(marble_velocity.clone())
+            .insert(marble_position.clone())
+            .insert(ObjectView::from_position(marble_position.clone()))
+            .insert_bundle(PbrBundle {
+                mesh: meshes.add(Mesh::from(shape::UVSphere {
+                    radius: 0.5,
+                    ..default()
+                })),
+                material: materials.add(StandardMaterial {
+                    base_color: Color::LIME_GREEN,
+                    ..default()
+                }),
+                transform: marble_position.clone().into(),
+                ..default()
+            });
     }
 }

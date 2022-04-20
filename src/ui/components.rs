982cc01cbcbae5bb6462161bcf64ad11cd982233
reiya0104 <reiya0104@ezweb.ne.jp>
2022-04-20 14:32:03 +0900

    add: ui controller

---
+++ src/ui/components.rs
@@ -0,0 +1,71 @@
+use bevy::prelude::*;
+
+// UIオブジェクト描画用コンポーネント
+#[derive(Debug, Default, Clone, Component)]
+pub(crate) struct UIView {
+    pub(crate) position: Vec2,
+}
+
+impl UIView {
+    pub(crate) fn new(position_vec2: Vec2) -> Self {
+        Self {
+            position: position_vec2,
+        }
+    }
+
+    pub(crate) fn from_ui_position(ui_position: UIPosition) -> Self {
+        Self {
+            position: ui_position.vec2,
+        }
+    }
+}
+
+impl From<UIView> for Rect<Val> {
+    fn from(ui_view: UIView) -> Self {
+        Self {
+            right: Val::Px(ui_view.position.x),
+            bottom: Val::Px(ui_view.position.y),
+            ..default()
+        }
+    }
+}
+
+#[derive(Debug, Default, Clone, Component)]
+pub(crate) struct UIPosition {
+    pub(crate) vec2: Vec2,
+}
+
+impl UIPosition {
+    pub(crate) fn new(x: f32, y: f32) -> Self {
+        Self {
+            vec2: Vec2::new(x, y),
+        }
+    }
+}
+
+impl From<UIPosition> for Rect<Val> {
+    fn from(position: UIPosition) -> Self {
+        Self {
+            right: Val::Px(position.vec2.x),
+            bottom: Val::Px(position.vec2.y),
+            ..default()
+        }
+    }
+}
+
+#[derive(Debug, Default, Clone, Component)]
+pub(crate) struct UISquareSize {
+    pub(crate) size: f32,
+}
+
+impl From<UISquareSize> for Size<Val> {
+    fn from(ui_square_size: UISquareSize) -> Self {
+        Self::new(Val::Px(ui_square_size.size), Val::Px(ui_square_size.size))
+    }
+}
+
+#[derive(Component)]
+pub(crate) struct MouseControllerBase;
+
+#[derive(Component)]
+pub(crate) struct MouseControllerMain;
---
+++ src/ui/systems.rs
@@ -0,0 +1,121 @@
+use crate::*;
+use bevy::prelude::*;
+
+pub(crate) fn setup_ui(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    asset_server: Res<AssetServer>,
+) {
+    // base
+    let base_size = UISquareSize { size: 150.0 };
+    let base_position = UIPosition {
+        vec2: Vec2::new(50.0, 50.0),
+    };
+
+    // main
+    let main_size = UISquareSize { size: 100.0 };
+    let main_position = UIPosition {
+        vec2: Vec2::new(0.0, 0.0),
+    };
+
+    // mouse controller
+    commands
+        // base
+        .spawn()
+        .insert(base_size.clone())
+        .insert(base_position.clone())
+        .insert(UIView::from_ui_position(base_position.clone()))
+        .insert_bundle(ImageBundle {
+            style: Style {
+                size: base_size.into(),
+                position_type: PositionType::Absolute,
+                position: base_position.into(),
+                // position: Rect(Val::Px(10.0), Val::Px(0.0)),
+                ..default()
+            },
+            image: asset_server.load("image/mouse_controller_base.png").into(),
+            ..default()
+        })
+        .insert(MouseControllerBase)
+        .with_children(|parent| {
+            // main
+            parent
+                .spawn()
+                .insert(main_size.clone())
+                .insert(main_position.clone())
+                .insert(UIView::from_ui_position(main_position.clone()))
+                .insert_bundle(ImageBundle {
+                    style: Style {
+                        size: main_size.into(),
+                        position_type: PositionType::Absolute,
+                        position: main_position.into(),
+                        ..default()
+                    },
+                    image: asset_server.load("image/mouse_controller_main.png").into(),
+                    ..default()
+                })
+                .insert(MouseControllerMain);
+        });
+}
+
+pub(crate) fn update_mouse_coltroller_main_position(
+    mut cursor_moved_events: EventReader<CursorMoved>,
+    windows: Res<Windows>,
+    query_base: Query<
+        (&UIPosition, &UISquareSize),
+        (With<MouseControllerBase>, Without<MouseControllerMain>),
+    >,
+    mut query_main: Query<
+        (&mut UIPosition, &UISquareSize),
+        (With<MouseControllerMain>, Without<MouseControllerBase>),
+    >,
+) {
+    // カーソルの位置にあった main_position に更新する
+
+    let max_controller_radius = 45.0;
+
+    for event in cursor_moved_events.iter() {
+        // カーソルの位置を取得
+        let cursor_position = event.position;
+
+        // ウィンドウ幅を取得するために window を取得
+        if let Some(window) = windows.iter().last() {
+            let iter = query_base.iter().zip(query_main.iter_mut());
+
+            if let Some(((base_position, base_size), (mut main_position, main_size))) = iter.last()
+            {
+                let base_center = Vec2::splat(base_size.size) / 2.0 + base_position.vec2;
+
+                let cursor_position_relative =
+                    Vec2::new(window.width() - cursor_position.x, cursor_position.y) - base_center;
+
+                main_position.vec2 = if cursor_position_relative.length() > max_controller_radius {
+                    max_controller_radius * cursor_position_relative.normalize()
+                } else {
+                    cursor_position_relative
+                };
+            }
+        }
+    }
+}
+
+pub(crate) fn update_ui_view_by_main_position(
+    query_base: Query<&UISquareSize, (With<MouseControllerBase>, Without<MouseControllerMain>)>,
+    query_main: Query<&UISquareSize, (With<MouseControllerMain>, Without<MouseControllerBase>)>,
+    mut query: Query<(&mut UIView, &UIPosition), With<MouseControllerMain>>,
+) {
+    for (mut ui_view, ui_position) in query.iter_mut() {
+        let iter = query_base.iter().zip(query_main.iter());
+        if let Some((base_size, main_size)) = iter.last() {
+            ui_view.position =
+                ui_position.vec2 + Vec2::splat(base_size.size - main_size.size) / 2.0;
+        }
+    }
+}
+
+pub(crate) fn update_ui_style(mut query: Query<(&mut Style, &UIView), With<MouseControllerMain>>) {
+    for (mut style, ui_view) in query.iter_mut() {
+        style.position = ui_view.clone().into();
+    }
+}

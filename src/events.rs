2f99fc7c42a4ea990ae2e2ce508388fb75ed70d6
reiya0104 <reiya0104@ezweb.ne.jp>
2022-04-23 02:46:25 +0900

    Revert "add: 衝突時の天板上の衝突点の速さを取得(Revert)"
    This reverts commit eb8e2486d48d2fff6b7ff891b2388ca2ca71a761.

--- src/events.rs
+++ src/events.rs
@@ -5,11 +5,4 @@ pub(crate) struct MarbleCreatedEvent {
     pub(crate) entity: Entity,
 }
 
-pub(crate) struct CollisionEvent {
-    pub(crate) position: Vec3,
-    /// entity of board 
-    /// 法線ベクトル(normal vector)の entity にするか迷いましたが，
-    /// 天板(board) にある Rotation と PreviousRotation にアクセスしたいので，
-    /// board_entity にしました．
-    pub(crate) board_entity: Entity,
-}
+pub(crate) struct CollisionEvent {}
